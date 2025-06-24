92893a4e88a8863c7d9b22b8b3ab3b76cb8322ca
Eric Elmoznino <eric.elmoznino@gmail.com>
2025-06-23 20:42:11 -0400

    Initial commit

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Welcome to my blog!",
+        "excerpt":"I’ll be posting stuff here that isn’t suitable for paper publications!      ","categories": [],
+        "tags": [],
+        "url": "/blog/2025/06/23/welcome/",
+        "teaser": null
+      }]
