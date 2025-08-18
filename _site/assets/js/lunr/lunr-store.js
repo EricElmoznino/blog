53c70620b84083339cfd5c519f1a026cc822659f
Eric Elmoznino <eric.elmoznino@gmail.com>
2025-08-18 16:38:51 -0400

    Added blog post on defining compositionality

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": [],
         "url": "/blog/2025/06/23/welcome/",
         "teaser": null
+      },{
+        "title": "Defining and quantifying compositional structure",
+        "excerpt":"What is compositionality? For those of us working in AI or cognitive neuroscience this question can appear easy at first, but becomes increasingly perplexing the more we think about it. We aren’t short on intuitions: we know that compositionality has something to do with reuse of parts, combinatorial expressive power,...","categories": [],
+        "tags": [],
+        "url": "/blog/2025/08/18/defining-compositionality/",
+        "teaser": null
       }]
